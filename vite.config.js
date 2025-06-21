8a99d86f1072b1f9edbe41d562af315e5c5a0ed2
unknown <sreejadas0405@gmail.com>
2025-06-21 22:13:27 +0530

    Updated vite config and build for github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/photobooth-web/', // <-- Add this line (replace with your repo name)
   plugins: [react()],
 })
